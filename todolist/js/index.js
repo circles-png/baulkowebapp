59e11884c710a81805c0ef149238d9806b2863d9
ML-10 <matthew.li10@education.nsw.gov.au>
2021-06-10 19:47:43 +1000

    to do list more

--- todolist/js/index.js
+++
@@ -1,9 +0,0 @@
-/*Downloaded from https://www.codeseek.co/RonaldsVilcins/simple-bootstrap-to-do-list-template-iJxGB */
-$(document).ready(function () {
-    $('button').click(function () {
-        $('#todo').append("<ul>" + $("input[name=task]").val() + " <a href='#' class='close' aria-hidden='true'>&times;</a></ul>");
-    });
-    $("body").on('click', '#todo a', function () {
-        $(this).closest("ul").remove();
-    });
-});
