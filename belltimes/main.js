4517c3ce07c718c687f6f1a2308f48275a7660af
Matthew Li <matthew.li10@education.nsw.gov.au>
2021-06-10 18:19:23 +1000

    credit

--- belltimes/main.js
+++ belltimes/main.js
@@ -3,7 +3,7 @@ var day = newday.getDay();
 var currentsec = newday.getHours() * 3600 + newday.getMinutes() * 60 + newday.getSeconds();
 var daydata;
 var numberofperiods;
-var table = "<table class='table my-5 fs-5'>";
+var table = "<table class='table my-5 fs-5'><caption>Code by: Matthew Wu :3</caption>";
 var periodtimename = [];
 var theme = 0;
 var weekend = 1;
